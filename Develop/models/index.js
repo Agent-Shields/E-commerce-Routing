b603d8770930d614018a3d90009ddd4110f95f85
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-02-26 14:58:44 -0600

    defined models and associations between them

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+  onDelete: 'SET NULL'
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'product_id'
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'product_tag',
+  foreignKey: 'product_id',
+  onDelete: 'SET NULL'
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'product_tag',
+  foreignKey: 'tag_id',
+  onDelete: 'SET NULL'
+})
 module.exports = {
   Product,
   Category,
