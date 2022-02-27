c8c92cfdbad126967578c11e37310a7740608eff
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-02-27 11:48:03 -0600

    CRUD operations for each route functional

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -103,6 +103,18 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbPostData => {
+    res.json(dbPostData)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 module.exports = router;
